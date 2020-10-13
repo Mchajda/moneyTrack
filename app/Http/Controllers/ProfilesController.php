f67a97fdb40872a49da886ee253a0d1e77cb2a36
Maciej <48365963+Mchajda@users.noreply.github.com>
2020-10-13 09:47:23 +0200

    summary expanding

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Expense;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -22,7 +23,6 @@ class ProfilesController extends Controller
 
         auth()->user()->profile()->update($data);
         return redirect()->route('home');
-        //dd(request()->all());
     }
 
     public function showExpenses(){
@@ -41,6 +41,31 @@ class ProfilesController extends Controller
 
     public function showSummary(){
         $user = User::find(auth()->user()->id);
+        $months = ['styczeń', 'luty', 'marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec', 'sierpień', 'wrzesień', 'październik', 'listopad', 'grudzień'];
+        $this_month = $months[date('m')-1];
+        $categories = Category::all();
+        $this_month_expenses = [];
+        $expenses = Expense::where('user_id', auth()->user()->id)->where('direction', 'expense')->get();
+
+        foreach($categories as $cat){
+            $this_month_expenses[$cat->id] = 0;
+            foreach($expenses as $expense){
+                if($expense->category == $cat->category_name)
+                    $this_month_expenses[$cat->id] += $expense->amount;
+            }
+        }
+
+        /*
+         * wydatki posegregowane na miesiace np do rocznego zestawienia
+         */
+        for( $i=0 ; $i<12 ; $i++){
+            $monthly_sum = 0;
+            foreach($expenses as $expense){
+                if(\Carbon\Carbon::parse($expense->date)->format('m') == $i)
+                    $monthly_sum += $expense->amount;
+            }
+            $monthly_expenses[$i] = $monthly_sum;
+        }
 
         /*
         $from = date('2020-10-01');
@@ -48,11 +73,13 @@ class ProfilesController extends Controller
         $expenses = Expense::whereBetween('date', [$from, $to])->get();
         */
 
-        $expenses = Expense::where('user_id', auth()->user()->id)->where('direction', 'expense')->get();
-
         return view('profile.summary', [
             'user' => $user,
             'expenses' => $expenses,
+            'month' => $this_month,
+            'this_month' => $this_month_expenses,
+            'monthly_expenses' => $monthly_expenses,
+            'categories' => $categories,
         ]);
     }
 }
--- resources/views/profile/summary.blade.php
+++ resources/views/profile/summary.blade.php
@@ -2,13 +2,17 @@
 
 @section('content')
 <div class="container">
-    <h3>Oto podsumowanie towich wydatków miesiąca <?php echo date('m')?></h3>
+    <h3>Oto podsumowanie twoich wydatków miesiąca {{ $month }}</h3>
+    poprzedniego miesiąca wydałeś {{ $monthly_expenses[date('m')-1] }} zł
+    <hr>
 </div>
 <div class="container">
-    @foreach($expenses as $expense)
-        @if(\Carbon\Carbon::parse($expense->date)->format('M') == 'Oct')
-            {{ $expense->title }}
-        @endif
+    @foreach($categories as $cat)
+        <div class="d-flex justify-content-between">
+            <div><b>{{ $cat->category_name }}</b>: {{ $this_month[$cat->id] }}</div>
+            <div>zł</div>
+        </div>
+        <hr>
     @endforeach
 </div>
 @endsection
