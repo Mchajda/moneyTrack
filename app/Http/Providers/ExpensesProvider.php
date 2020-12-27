b588cb59bfc908613d82cae747d731ca69f9ab42
Maciej <48365963+Mchajda@users.noreply.github.com>
2020-12-27 18:38:05 +0100

    Update ExpensesProvider.php

--- app/Http/Providers/ExpensesProvider.php
+++ app/Http/Providers/ExpensesProvider.php
@@ -12,7 +12,7 @@ class ExpensesProvider implements ExpensesProviderInterface
 
     public function getAll($user_id)
     {
-        return Expense::where('user_id', $user_id)->orderBy('date', 'DESC')->orderBy('created_at', 'DESC')->get();
+        return Expense::where('user_id', $user_id)->where('direction', 'expense')->orderBy('date', 'DESC')->orderBy('created_at', 'DESC')->get();
     }
 
     public function getAllByCategory($user_id, $category, $month)
